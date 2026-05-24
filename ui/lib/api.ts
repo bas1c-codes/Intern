4733f9b874972b923404cd52e25bf3eccf9ea0f6
Adhul Krishna <76802372+bas1c-codes@users.noreply.github.com>
2026-05-25 00:37:32 +0530

    Set default API_URL to '/api'
    Change API_URL to a default value of '/api'.

--- ui/lib/api.ts
+++ ui/lib/api.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosError } from 'axios';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL;
+const API_URL = "/api";
 
 if (!API_URL) {
   console.warn('NEXT_PUBLIC_API_URL environment variable is not set');
